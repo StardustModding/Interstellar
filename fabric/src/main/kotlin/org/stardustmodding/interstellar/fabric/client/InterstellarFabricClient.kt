baa42a04d74c856978e6917d02e5e3e1b4070678
Jacob Sapoznikow <jacob1coder@gmail.com>
2024-06-02 22:44:27 -0700

    oops

--- fabric/src/main/kotlin/org/stardustmodding/interstellar/fabric/client/InterstellarFabricClient.kt
+++ fabric/src/main/kotlin/org/stardustmodding/interstellar/fabric/client/InterstellarFabricClient.kt
@@ -1,9 +1,12 @@
 package org.stardustmodding.interstellar.fabric.client
 
 import net.fabricmc.api.ClientModInitializer
+import org.stardustmodding.dynamicdimensions.fabric.client.DynamicDimensionsFabricClient
 
 object InterstellarFabricClient: ClientModInitializer {
+    private val dyndims = DynamicDimensionsFabricClient()
+
     override fun onInitializeClient() {
-        // Nothing for now...
+        dyndims.onInitializeClient()
     }
 }
